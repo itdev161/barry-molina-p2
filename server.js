7b395c669c4038a2848a15077b2618f8c3b17c46
Barry Molina <bazzaboy@gmail.com>
2020-09-15 13:13:25 -0500

    Add POST endpoint

--- server.js
+++ server.js
@@ -6,8 +6,25 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({ extended: false }));
+
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
+
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
