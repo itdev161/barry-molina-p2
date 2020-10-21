ea90b9c0700bacec86dec2b7d3e6d32af42d7567
Barry Molina <bazzaboy@gmail.com>
2020-10-20 19:26:48 -0500

    Register User

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
 
 // Initialize express application
 const app = express();
@@ -35,12 +37,33 @@ app.post('/api/users',
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         } else {
-            res.send(req.body);
+            const { name, email, password } = req.body;
+            try {
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'User already exists'}] });
+                }
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+                
+                await user.save();
+                res.send('User successfully registered');
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
         }
     }
 );
